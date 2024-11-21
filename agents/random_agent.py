ce2b42c02ca5c8bdd2471f9d8547a93e0e65a815
dania0x4C <ljm31881004@gmail.com>
2024-11-21 12:44:08 +0900

    모듈화

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -1,9 +1,10 @@
 import random
 
+
 class RandomAgent:
     def __init__(self, action_space):
         self.action_space = action_space
 
-    def choose_action(self):
-        """랜덤하게 행동을 선택"""
-        return tuple(random.randint(0, n - 1) for n in self.action_space.nvec)
+    def select_action(self):
+        """랜덤한 행동 선택"""
+        return self.action_space.sample()
---
+++ data/vehicle_data.py
@@ -0,0 +1,9 @@
+vehicle_data = [
+    {"x": 3, "y": 1, "length": 2, "orientation": "H", "movable": True},
+    {"x": 4, "y": 2, "length": 2, "orientation": "H", "movable": True},
+    {"x": 1, "y": 3, "length": 3, "orientation": "V", "movable": True},
+    {"x": 1, "y": 4, "length": 3, "orientation": "H", "movable": True},
+    {"x": 6, "y": 3, "length": 3, "orientation": "H", "movable": True},
+    {"x": 2, "y": 6, "length": 2, "orientation": "V", "movable": True},
+    {"x": 4, "y": 6, "length": 3, "orientation": "V", "movable": True}
+]
